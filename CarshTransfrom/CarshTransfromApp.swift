f421ffdb51266ebd266ec113fc85c73a9cea5962
jianglong <jianglong@youloft.com>
2025-04-25 10:25:25 +0800

    Initial Commit

---
+++ CarshTransfrom/CarshTransfromApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarshTransfromApp.swift
+//  CarshTransfrom
+//
+//  Created by LongJiang on 2025/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CarshTransfromApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
